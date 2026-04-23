8772720585f81740ff3328100d4dae4fbcda7b5a
Haziel Garcia <reynagarciahaziel@hotmail.com~>
2026-04-23 11:47:11 -0600

    Restaurando estructura: Gestor de Actividades y Agenda de Contactos

--- Programacion-Web-2/gestor-tareas-angular/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-// Routes es el tipo que representa el arreglo de rutas de Angular.
-import { Routes } from '@angular/router';
-
-// En este ejemplo no vamos a navegar entre varias páginas.
-// Aun así se deja el archivo para mostrar la estructura correcta.
-export const routes: Routes = [];
